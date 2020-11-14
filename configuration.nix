c39b5de8d95adce2d733fc1dd7420d3487f0bff7
Paweł Placzyński <placzynski.pawel@gmail.com>
2020-11-14 20:07:24 +0100

    some improovements

--- configuration.nix
+++ configuration.nix
@@ -11,54 +11,53 @@
 
   # system settings
   networking.hostName = "vm-nixos";
+  networking.networkmanager.enable = true;
   time.timeZone = "Europe/Warsaw";
   i18n.defaultLocale = "pl_PL.UTF-8";
 
   nixpkgs.config.allowUnfree = true;
-  environment.systemPackages = [
-    pkgs.bash
-    pkgs.ctags
-    pkgs.curl
-    pkgs.entr
-    pkgs.fish
-    pkgs.fzf
-    pkgs.git
-    pkgs.gnumake
-    pkgs.mutt
-    pkgs.silver-searcher
-    pkgs.tig
-    pkgs.tmux
-    pkgs.vim
+  environment.systemPackages = with pkgs; [
+    bash
+    ctags
+    curl
+    entr
+    fish
+    fzf
+    git
+    gnumake
+    mutt
+    silver-searcher
+    tig
+    tmux
+    vim
 
-    pkgs.dunst
-    pkgs.feh
-    pkgs.google-chrome
-    pkgs.haskellPackages.xmonad
-    pkgs.haskellPackages.xmonad-contrib
-    pkgs.haskellPackages.xmonad-extras
-    pkgs.keepassxc
-    pkgs.networkmanager
-    pkgs.paper-icon-theme
-    pkgs.rofi
-    pkgs.rxvt-unicode
-    pkgs.xclip
-    pkgs.xdotool
-    pkgs.xmobar
-    pkgs.xmonad-with-packages
-    pkgs.xorg.xkill
+    dunst
+    feh
+    google-chrome
+    haskellPackages.xmonad
+    haskellPackages.xmonad-contrib
+    haskellPackages.xmonad-extras
+    keepassxc
+    paper-icon-theme
+    rofi
+    rxvt-unicode
+    xclip
+    xdotool
+    xmobar
+    xmonad-with-packages
+    xorg.xkill
   ];
 
   users.users = {
     placek = {
       isNormalUser = true;
-      extraGroups = [ "wheel" ];
+      extraGroups = [ "wheel" "networkmanager" ];
       packages = [];
     };
   };
 
   services.xserver = {
     libinput.enable = true;
-    libinput.naturalScrolling = true;
     windowManager.xmonad = {
       enableContribAndExtras = true;
       haskellPackages = pkgs.haskell.packages.ghc865;
@@ -76,7 +75,6 @@
     enable = true;
   };
 
-  # use `~/.Xresources` to change fonts and other display settings
   fonts = {
     fonts = [
       pkgs.ubuntu_font_family
