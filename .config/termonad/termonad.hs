b77a0d87a5b9b28853926762064e8944ab9acb46
Paweł Placzyński <placzynski.pawel@gmail.com>
2020-11-20 14:15:46 +0100

    minor change of termonad colors

--- .config/termonad/termonad.hs
+++ .config/termonad/termonad.hs
@@ -1,7 +1,3 @@
--- | This is an example Termonad configuration that shows how to use the
--- 'ColourExtension' to set colours for your terminal.  See the project-wide
--- README.md for how to use Termonad Haskell configuration scripts.
-
 module Main where
 
 import Data.Maybe (fromMaybe)
@@ -18,10 +14,6 @@ import Termonad.Config.Colour
   , defaultLightColours, foregroundColour, palette, mkList8, unsafeMkList8
   )
 
--- This is our main 'TMConfig'.  It holds all of the non-colour settings
--- for Termonad.
---
--- This shows how a few settings can be changed.
 myTMConfig :: TMConfig
 myTMConfig =
   defaultTMConfig
@@ -34,38 +26,30 @@ myTMConfig =
           }
     }
 
--- This is our 'ColourConfig'.  It holds all of our colour-related settings.
 myColourConfig :: ColourConfig (AlphaColour Double)
 myColourConfig =
   defaultColourConfig
-    -- Set the cursor background colour.  This is the normal colour of the
-    -- cursor.
-    { cursorBgColour = Set (createColour 120 80 110) -- purple
-    -- Set the default foreground colour of text of the terminal.
-    , foregroundColour = Set (createColour 220 180 210) -- light pink
-    -- Set the extended palette that has 8 colours standard colors and then 8
-    -- light colors.
+    -- normal colour of the cursor
+    { cursorBgColour = Set (createColour 120 80 110)
+    -- foreground colour of text of the terminal
+    , foregroundColour = Set (createColour 220 180 210)
+    -- extended palette that has 8 colours standard colors and then 8 light colors.
     , palette = ExtendedPalette myStandardColours
                                 (fromMaybe defaultLightColours myLightColours)
     }
   where
-    -- This is a an example of creating a linked-list of colours,
-    -- This function uses an unsafe method for generating the list.
-    -- An exception will be thrown if your list does not have 8 elements.
     myStandardColours :: List8 (AlphaColour Double)
     myStandardColours = unsafeMkList8
-      [ createColour  40  30  20 -- dark brown (used as background colour)
-      , createColour 180  30  20 -- red
-      , createColour  40 160  20 -- green
-      , createColour 180 160  20 -- dark yellow
-      , createColour  40  30 120 -- dark purple
+      [ createColour  44  62  80 -- background
+      , createColour 231  76  60 -- red
+      , createColour  46 204 113 -- green
+      , createColour 241 196  15 -- yellow
+      , createColour 155  89 182 -- magenta
       , createColour 180  30 120 -- bright pink
-      , createColour  40 160 120 -- teal
+      , createColour  26 188 156 -- cyan
       , createColour 180 160 120 -- light brown
       ]
 
-    -- This is an example of creating a linked-list of colours with a type
-    -- safe method. mkList8 produces a Maybe value which must be handled explicitely.
     myLightColours :: Maybe (List8 (AlphaColour Double))
     myLightColours = mkList8
         [ createColour  70  60  50 -- brown
@@ -80,11 +64,6 @@ myColourConfig =
 
 main :: IO ()
 main = do
-  -- First, create the colour extension based on 'myColourConfig'.
   myColourExt <- createColourExtension myColourConfig
-
-  -- Update 'myTMConfig' with our colour extension.
   let newTMConfig = addColourExtension myTMConfig myColourExt
-
-  -- Start Termonad with our updated 'TMConfig'.
   start newTMConfig
