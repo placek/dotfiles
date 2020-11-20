9bd6803e2be7c73b13befaf5db51702bb79aff5a
Paweł Placzyński <placzynski.pawel@gmail.com>
2020-11-20 14:52:54 +0100

    font config for termonad

--- .config/termonad/termonad.hs
+++ .config/termonad/termonad.hs
@@ -6,6 +6,8 @@ import Termonad
   ( CursorBlinkMode(CursorBlinkModeOff), Option(Set)
   , ShowScrollbar(ShowScrollbarNever), TMConfig, confirmExit, cursorBlinkMode
   , defaultConfigOptions, defaultTMConfig, options, showMenu, showScrollbar
+  , FontConfig, FontSize(FontSizePoints), defaultFontConfig, fontConfig
+  , fontFamily, fontSize
   , start
   )
 import Termonad.Config.Colour
@@ -23,7 +25,15 @@ myTMConfig =
           , confirmExit = False
           , showMenu = False
           , cursorBlinkMode = CursorBlinkModeOff
+          , fontConfig = fontConf
           }
+    }
+
+fontConf :: FontConfig
+fontConf =
+  defaultFontConfig
+    { fontFamily = "Iosevka"
+    , fontSize = FontSizePoints 12
     }
 
 myColourConfig :: ColourConfig (AlphaColour Double)
