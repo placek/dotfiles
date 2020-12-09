19f23888fe75749d9fef38dfd966083dd6c6e5cc
Paweł Placzyński <placzynski.pawel@gmail.com>
2020-12-09 11:42:12 +0100

    add potential dotfiles service

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -192,4 +192,24 @@
       };
     };
   };
+
+  # systemd.user.services.dotfiles = {
+  #   description = "Dot files synchronization";
+  #   enable = true;
+  #   serviceConfig = {
+  #     Environment = [
+  #       "DOTFILES_URL=git@github.com:placek/dotfiles.git"
+  #       "DOTFILES_DIR=.config/dotfiles"
+  #     ];
+  #     Type             = "oneshot";
+  #     WorkingDirectory = "/home/placek/.config/dotfiles";
+  #     RemainAfterExit  = "yes";
+  #     ExecStartPre     = "-${pkgs.git}/bin/git clone --recurse-submodules $DOTFILES_URL $DOTFILES_DIR";
+  #     ExecStart        = "${pkgs.stow}/bin/stow --target=$HOME --dir=$DOTFILES_DIR --stow";
+  #     ExecReloadPre    = "-${pkgs.git}/bin/git pull ";
+  #     ExecReload       = "${pkgs.stow}/bin/stow --target=$HOME --dir=$DOTFILES_DIR --restow";
+  #     ExecStop         = "${pkgs.stow}/bin/stow --target=$HOME --dir=$DOTFILES_DIR --delete";
+  #   };
+  #   wantedBy = [ "default.target" ];
+  # };
 }
