07ad8ef4735c8a0ea986df639929f683606f9bd6
Paweł Placzyński <placzynski.pawel@gmail.com>
2020-12-10 14:05:57 +0100

    fix

--- nix/systemd.nix
+++ nix/systemd.nix
@@ -12,9 +12,8 @@
       Type             = "oneshot";
       RemainAfterExit  = "yes";
       ExecStartPre     = "-${pkgs.git}/bin/git clone --recurse-submodules $DOTFILES_URL $HOME/$DOTFILES_DIR";
-      ExecReloadPre    = "-${pkgs.git}/bin/git -C $HOME/$DOTFILES_DIR pull --ff-only origin master";
       ExecStart        = "${pkgs.bash}/bin/bash -c 'ls -1 $DOTFILES_DIR/home | xargs ${pkgs.stow}/bin/stow --target=$HOME --dir=$HOME/$DOTFILES_DIR --stow'";
-      ExecReload       = "${pkgs.bash}/bin/bash -c 'ls -1 $DOTFILES_DIR/home | xargs ${pkgs.stow}/bin/stow --target=$HOME --dir=$HOME/$DOTFILES_DIR --restow'";
+      ExecReload       = "${pkgs.bash}/bin/bash -c '${pkgs.git}/bin/git -C $HOME/$DOTFILES_DIR pull --ff-only origin master | ls -1 $DOTFILES_DIR/home | xargs ${pkgs.stow}/bin/stow --target=$HOME --dir=$HOME/$DOTFILES_DIR --restow'";
       ExecStop         = "${pkgs.bash}/bin/bash -c 'ls -1 $DOTFILES_DIR/home | xargs ${pkgs.stow}/bin/stow --target=$HOME --dir=$HOME/$DOTFILES_DIR --delete'";
     };
     wantedBy = [ "default.target" ];
