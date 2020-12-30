73200db1abaec0ea7cdef56667bafd78ee8f6b92
Paweł Placzyński <placzynski.pawel@gmail.com>
2020-12-30 15:43:03 +0100

    better building solution for projects

--- nix/derivations/projects/default.nix
+++ nix/derivations/projects/default.nix
@@ -2,14 +2,14 @@
 
 pkgs.stdenv.mkDerivation rec {
   name = "projects-${version}";
-  version = "1.0";
+  version = "1.1";
 
   src = ../../sources/projects;
 
   buildPhase = "";
   installPhase = ''
     mkdir -p $out/bin
-    cp $src/binary $out/bin/projects
-    chmod +x $out/bin/projects
+    cp $src/* $out/bin
+    chmod +x $out/bin/*
   '';
 }
