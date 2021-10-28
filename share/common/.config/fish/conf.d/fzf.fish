42080ca0bf938cafa9ac43376a25d44be5e26494
Paweł Placzyński <placzynski.pawel@gmail.com>
2021-10-28 08:30:02 +0200

    fix fzf bindings for fish

--- share/common/.config/fish/conf.d/fzf.fish
+++ share/common/.config/fish/conf.d/fzf.fish
@@ -174,20 +174,16 @@ function __fzf_search_shell_variables --description "Search and inspect shell va
   commandline --function repaint
 end
 
-if not set --query fzf_fish_custom_keybindings
-  bind \cf '__fzf_search_current_dir'
-  bind \cr '__fzf_search_history'
-  bind \cv '__fzf_search_shell_variables'
-  bind \e\cl '__fzf_search_git_log'
-  bind \e\cs '__fzf_search_git_status'
+bind \cf '__fzf_search_current_dir'
+bind \cr '__fzf_search_history'
+bind \cv '__fzf_search_shell_variables'
+bind \e\cl '__fzf_search_git_log'
+bind \e\cs '__fzf_search_git_status'
 
-  if test "$fish_key_bindings" = 'fish_vi_key_bindings'
-    bind --mode insert \cf '__fzf_search_current_dir'
-    bind --mode insert \cr '__fzf_search_history'
-    bind --mode insert \cv '__fzf_search_shell_variables'
-    bind --mode insert \ct '__fzf_search_todos'
-    bind --mode insert \e\cl '__fzf_search_git_log'
-    bind --mode insert \e\cs '__fzf_search_git_status'
-    bind --mode insert \e\cb '__fzf_search_git_branch'
-  end
-end
+bind --mode insert \cf '__fzf_search_current_dir'
+bind --mode insert \cr '__fzf_search_history'
+bind --mode insert \cv '__fzf_search_shell_variables'
+bind --mode insert \ct '__fzf_search_todos'
+bind --mode insert \e\cl '__fzf_search_git_log'
+bind --mode insert \e\cs '__fzf_search_git_status'
+bind --mode insert \e\cb '__fzf_search_git_branch'
