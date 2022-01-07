31e1a14437e508f27d5aa0c569d8dbd4c7edd219
Paweł Placzyński <placzynski.pawel@gmail.com>
2022-01-07 10:33:30 +0100

    [xmonad] phrase prompt simplification

--- share/xserver/.xmonad/lib/Prompt/Phrase.hs
+++ share/xserver/.xmonad/lib/Prompt/Phrase.hs
@@ -3,37 +3,29 @@ module Prompt.Phrase (phrasePrompt) where
 import           System.Directory (getHomeDirectory)
 import           System.FilePath  (combine)
 import           XMonad.Core
-import           XMonad.Prompt    (XPConfig, XPrompt, commandToComplete,
-                                   getNextCompletion, mkXPrompt, nextCompletion,
-                                   searchPredicate, showXPrompt)
+import           XMonad.Prompt
 
-type Predicate = String -> String -> Bool
-
-getPhraseCompl :: [String] -> Predicate -> String -> IO [String]
-getPhraseCompl compls p s = return $ filter (p s) compls
-
-data Phrase = Phrase
+data Phrase = Phrase XPConfig
 
 instance XPrompt Phrase where
-  showXPrompt          Phrase = "Phrase"
-  commandToComplete _ c       = c
+  showXPrompt (Phrase _) = "Phrase"
+
+  commandToComplete _ c = c
+
+  nextCompletion _ = getNextCompletion
+
+  completionFunction (Phrase c) s = do
+    home    <- getHomeDirectory
+    phrases <- readFile $ combine home ".abbr"
+    return $ filter ((searchPredicate c) s) (lines phrases)
+
+  modeAction _ a _ = spawn $ "echo -n \"" ++ escapeQuote a ++ "\" | xdotool type --clearmodifiers --file -"
 
 phrasePrompt :: XPConfig -> X ()
-phrasePrompt xpconfig = do
-  phrases <- io getPhrases
-  mkXPrompt Phrase xpconfig (getPhraseCompl phrases $ searchPredicate xpconfig) selectPhrase
-
-selectPhrase :: String -> X ()
-selectPhrase phrase = spawn $ "echo -n \"" ++ escapeQuote phrase ++ "\"|xdotool type --clearmodifiers --file -"
+phrasePrompt xpconfig = mkXPromptWithModes [XPT $ Phrase xpconfig] xpconfig
 
 escapeQuote :: String -> String
 escapeQuote = concatMap escape
   where escape :: Char -> String
         escape '"' = "\\\""
         escape x   = [x]
-
-getPhrases :: IO [String]
-getPhrases = do
-  home    <- getHomeDirectory
-  phrases <- readFile $ combine home ".abbr"
-  return . lines $ phrases
