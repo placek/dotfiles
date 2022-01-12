a297da9d50edd3c96e90150c0100a762e51944e2
Paweł Placzyński <placzynski.pawel@gmail.com>
2022-01-12 15:50:22 +0100

    [xmonad] fix for bad clipboard quoting

--- share/xserver/.xmonad/lib/Prompt/Clipboard.hs
+++ share/xserver/.xmonad/lib/Prompt/Clipboard.hs
@@ -2,7 +2,7 @@ module Prompt.Clipboard (clipboardPrompt) where
 
 import           XMonad.Core
 import           XMonad.Prompt
-import           XMonad.Util.Run (runProcessWithInput)
+import           XMonad.Util.Run (runProcessWithInput, safeSpawn)
 
 data Clipboard = Clipboard XPConfig
 
@@ -17,13 +17,7 @@ instance XPrompt Clipboard where
     clips <- runProcessWithInput "greenclip" ["print"] []
     return $ filter ((searchPredicate c) s) (lines clips)
 
-  modeAction _ a _ = spawn $ "greenclip print \"" ++ escapeQuote a ++ "\""
+  modeAction _ a _ = io $ safeSpawn "greenclip" ["print", a]
 
 clipboardPrompt :: XPConfig -> X ()
 clipboardPrompt xpconfig = mkXPromptWithModes [XPT $ Clipboard xpconfig] xpconfig
-
-escapeQuote :: String -> String
-escapeQuote = concatMap escape
-  where escape :: Char -> String
-        escape '"' = "\\\""
-        escape x   = [x]
