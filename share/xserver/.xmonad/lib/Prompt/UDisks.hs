790d4567d0c0938868f31358ab7e7cccf522cb0e
Paweł Placzyński <placzynski.pawel@gmail.com>
2022-01-17 13:55:51 +0100

    [xmonad] udisks prompt improovement

--- share/xserver/.xmonad/lib/Prompt/UDisks.hs
+++ share/xserver/.xmonad/lib/Prompt/UDisks.hs
@@ -1,10 +1,11 @@
 module Prompt.UDisks (udisksPrompt) where
 
+import           Data.List              (isPrefixOf)
+import           System.Posix.Env       (getEnv)
 import           XMonad.Core
 import           XMonad.Prompt          (XPConfig(..), XPrompt(..),  XPType(..), mkXPromptWithModes)
 import           XMonad.Prompt.Input    (inputPromptWithCompl)
-import           XMonad.Util.Run        (runProcessWithInput)
-import           Data.List              (isPrefixOf)
+import           XMonad.Util.Run        (runProcessWithInput, safeSpawn)
 
 import           Prompt.UDisks.Device
 
@@ -25,23 +26,37 @@ instance XPrompt UDisks where
 
   commandToComplete _ c = c
 
-  completionFunction (UDisks Unmount c) s = do
+  completionFunction (UDisks Mount c) s = do
     devices <- getDevices
-    let paths = devicePath <$> (filter isMountedPartition devices)
+    let paths = devicePath <$> (filter (hasFileSystem .&. isNotMounted) devices)
     return $ filter (searchPredicate c $ s) paths
 
-  completionFunction (UDisks LoopDelete c) s = do
+  completionFunction (UDisks Unmount c) s = do
     devices <- getDevices
-    let paths = devicePath <$> (filter isLoop devices)
+    mUser <- getEnv "USER"
+    case mUser of
+      Just user -> do
+        let paths = devicePath <$> (filter (hasFileSystem .&. isMounted user) devices)
+        return $ filter (searchPredicate c $ s) paths
+      Nothing -> return []
+
+  completionFunction (UDisks Unlock c) s = do
+    devices <- getDevices
+    let paths = devicePath <$> (filter (isNotMounted .&. isLuks) devices)
+    return $ filter (searchPredicate c $ s) paths
+
+  completionFunction (UDisks Lock c) s = do
+    devices <- getDevices
+    let paths = devicePath <$> (filter (isNotMounted .&. isLuks) devices)
     return $ filter (searchPredicate c $ s) paths
 
   completionFunction (UDisks LoopSetup c) s = do
     files <- compgenFiles s
     return $ filter (searchPredicate c $ s) (lines files)
 
-  completionFunction (UDisks _ c) s = do
+  completionFunction (UDisks LoopDelete c) s = do
     devices <- getDevices
-    let paths = devicePath <$> (filter isNotMountedPartition devices)
+    let paths = devicePath <$> (filter (isLoop .&. isNotMounted) devices)
     return $ filter (searchPredicate c $ s) paths
 
   modeAction (UDisks t c) a _ = actionFor t a
@@ -70,47 +85,36 @@ keyFilePrompt xpconfig = inputPromptWithCompl xpconfig "Select key file" compl
 
 -- actions
 mountAction :: String -> X ()
-mountAction path = do
-  output <- runUDiskCtl ["mount", "-b", path, "--no-user-interaction"]
-  notify output path "Mounting succeded" "Mounting failed"
+mountAction path = runUDiskCtl ["mount", "-b", path, "--no-user-interaction"] >>= notify "Mounted"
 
 unmountAction :: String -> X ()
-unmountAction path = do
-  output <- runUDiskCtl ["unmount", "-b", path, "--no-user-interaction"]
-  notify output path "Unmounting succeded" "Unmounting failed"
+unmountAction path = runUDiskCtl ["unmount", "-b", path, "--no-user-interaction"] >>= notify "Unmounted"
 
 unlockAction :: XPConfig -> String -> X ()
 unlockAction xpconfig path = do
   keyFilePath <- keyFilePrompt xpconfig
   case keyFilePath of
     Nothing     -> return ()
-    Just kfPath -> do
-                     output <- runUDiskCtl ["unlock", "-b", path, "--key-file", kfPath, "--no-user-interaction"]
-                     notify output path "Unlocking succeded" "Unlocking failed"
+    Just kfPath -> runUDiskCtl ["unlock", "-b", path, "--key-file", kfPath, "--no-user-interaction"] >>= notify "Unlocked"
 
 lockAction :: String -> X ()
-lockAction path = do
-  output <- runUDiskCtl ["lock", "-b", path, "--no-user-interaction"]
-  notify output path "Locking succeded" "Locking failed"
+lockAction path = runUDiskCtl ["lock", "-b", path, "--no-user-interaction"] >>= notify "Locked"
 
 loopSetupAction :: String -> X ()
-loopSetupAction path = do
-  output <- runUDiskCtl ["loop-setup", "-f", path, "--no-user-interaction"]
-  notify output path "Seting up loop succeded" "Seting up loop failed"
+loopSetupAction path = runUDiskCtl ["loop-setup", "-f", path, "--no-user-interaction"] >>= notify "Mapped"
 
 loopDeleteAction :: String -> X ()
-loopDeleteAction path = do
-  output <- runUDiskCtl ["loop-delete", "-b", path, "--no-user-interaction"]
-  notify output path "Deleting loop succeded" "Deleting loop failed"
+loopDeleteAction path = runUDiskCtl ["loop-delete", "-b", path, "--no-user-interaction"] >>= notify ""
 
 -- utils
 compgenFiles :: String -> IO String
 compgenFiles s = runProcessWithInput "bash" [] $ "bind 'set completion-ignore-case on'; compgen -A file -- " ++ s ++ "\n"
 
-notify :: String -> String -> String -> String -> X ()
-notify output path success failure = if isPrefixOf "Error" output then sendWarning else sendNotification
-  where sendNotification = spawn $ "notify-send -u normal \""   ++ success ++ "\" \"" ++ path ++ "\""
-        sendWarning      = spawn $ "notify-send -u critical \"" ++ failure ++ "\" \"" ++ path ++ "\""
+notify :: String -> String -> X ()
+notify prefix output = safeSpawn "notify-send" ["-u", kind, "-i", "/run/current-system/sw/share/icons/Paper/scalable/devices/drive-harddisk-usb-symbolic.svg", "UDisks", output]
+  where kind = if isPrefixOf prefix output
+               then "normal"
+               else "critical"
 
 runUDiskCtl :: [String] -> X String
 runUDiskCtl args = io $ runProcessWithInput "udisksctl" args []
@@ -120,7 +124,7 @@ parseDevice input = either (Left . show) Right (parse input input)
 
 getDevices :: IO [Device]
 getDevices = do
-  output <- runProcessWithInput "lsblk" ["-o", "PATH,TYPE,HOTPLUG,SIZE,MOUNTPOINT", "--pairs"] []
+  output <- runProcessWithInput "lsblk" ["-o", "PATH,TYPE,HOTPLUG,SIZE,MOUNTPOINT,FSTYPE", "--pairs"] []
   case sequence $ parseDevice <$> lines output of
     Left err -> print err >> fail err
     Right ds -> return ds
--- share/xserver/.xmonad/lib/Prompt/UDisks/Device.hs
+++ share/xserver/.xmonad/lib/Prompt/UDisks/Device.hs
@@ -1,32 +1,42 @@
-module Prompt.UDisks.Device (Device (..), parse, isLoop, isMountedPartition, isNotMountedPartition) where
+module Prompt.UDisks.Device (Device(..), parse, (.&.), isLoop, isMounted, isNotMounted, isLuks, hasFileSystem) where
 
-import qualified Text.Parsec as Parsec
-import           Data.List             (isPrefixOf)
+import qualified Text.Parsec      as Parsec
+import           Data.List                  (isPrefixOf)
+
+(.&.) :: (a -> Bool) -> (a -> Bool) -> a -> Bool
+a .&. b = \c -> a c && b c
 
 data Device =
   Device { devicePath :: String
          , deviceType :: String
          , isHotplug  :: Bool
          , size       :: String
-         , mountpoint :: Maybe String
+         , mountPoint :: Maybe String
+         , fsType     :: Maybe String
          } deriving Show
 
 isLoop :: Device -> Bool
 isLoop device = "loop" == deviceType device
 
-isMounted :: Device -> Bool
-isMounted device = case mountpoint device of
-                     Nothing -> False
-                     Just _  -> True
+isMounted :: String -> Device -> Bool
+isMounted user device = case mountPoint device of
+                          Just a    -> isPrefixOf ("/run/media/" ++ user) a
+                          otherwise -> False
 
-isLoopPartition :: Device -> Bool
-isLoopPartition device = isPrefixOf "/dev/loop" (devicePath device)
+isNotMounted :: Device -> Bool
+isNotMounted device = case mountPoint device of
+                        Nothing -> True
+                        Just _  -> False
 
-isMountedPartition :: Device -> Bool
-isMountedPartition device = isMounted device && (isHotplug device || isLoopPartition device)
+hasFileSystem :: Device -> Bool
+hasFileSystem device = case fsType device of
+                         Nothing -> False
+                         Just _  -> True
 
-isNotMountedPartition :: Device -> Bool
-isNotMountedPartition device = (not . isMounted $ device) && (isHotplug device || isLoopPartition device)
+isLuks :: Device -> Bool
+isLuks device = case fsType device of
+                  Just "crypto_LUKS" -> True
+                  otherwise          -> False
 
 parse = Parsec.parse deviceParser
 
@@ -42,7 +52,9 @@ deviceParser = do
   dSize <- sizeParser
   Parsec.spaces
   dMountpoint <- mountpointParser
-  return $ Device dPath dType dHotplug dSize dMountpoint
+  Parsec.spaces
+  dFSType <- fsTypeParser
+  return $ Device dPath dType dHotplug dSize dMountpoint dFSType
 
 pathParser :: Parsec.Parsec String () String
 pathParser = do
@@ -77,6 +89,15 @@ sizeParser = do
 mountpointParser :: Parsec.Parsec String () (Maybe String)
 mountpointParser = do
   Parsec.string "MOUNTPOINT=\""
+  value <- Parsec.many $ Parsec.noneOf "\""
+  Parsec.char '"'
+  return $ if null value
+           then Nothing
+           else Just value
+
+fsTypeParser :: Parsec.Parsec String () (Maybe String)
+fsTypeParser = do
+  Parsec.string "FSTYPE=\""
   value <- Parsec.many $ Parsec.noneOf "\""
   Parsec.char '"'
   return $ if null value
