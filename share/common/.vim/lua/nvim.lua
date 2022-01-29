efa9abc0fc991ba329af8640c6f8239f0ef17ce7
Paweł Placzyński <placzynski.pawel@gmail.com>
2022-01-29 22:27:50 +0100

    [neovim] reorganize config files

--- share/common/.vim/lua/nvim.lua
+++ share/common/.vim/lua/nvim.lua
@@ -1,38 +1,12 @@
-vim.opt.backup         = false
-vim.opt.clipboard      = "unnamedplus"
-vim.opt.cmdheight      = 1
-vim.opt.colorcolumn    = { "80", "160" }
-vim.opt.completeopt    = { "menu", "menuone", "noselect" }
 vim.opt.conceallevel   = 0
-vim.opt.cursorline     = true
-vim.opt.expandtab      = true
-vim.opt.fileencoding   = "utf-8"
-vim.opt.foldcolumn     = "1"
-vim.opt.foldmethod     = "manual"
-vim.opt.hlsearch       = true
-vim.opt.list           = true
-vim.opt.listchars      = "tab:» ,nbsp:␣,trail:·,extends:›,precedes:‹"
-vim.opt.mouse          = "a"
-vim.opt.number         = true
 vim.opt.numberwidth    = 2
 vim.opt.pumheight      = 10
 vim.opt.scrolloff      = 8
-vim.opt.shiftwidth     = 2
 vim.opt.showmode       = false
 vim.opt.sidescrolloff  = 8
-vim.opt.signcolumn     = "yes"
 vim.opt.smartindent    = true
-vim.opt.splitbelow     = true
-vim.opt.splitright     = true
-vim.opt.swapfile       = false
-vim.opt.tabstop        = 2
-vim.opt.timeoutlen     = 1000
 vim.opt.undofile       = true
-vim.opt.updatetime     = 300
-vim.opt.wrap           = false
 vim.opt.writebackup    = false
-
-vim.opt.shortmess:append "c"
 
 local opts = { noremap = true, silent = true }
 local term_opts = { silent = true }
@@ -77,12 +51,8 @@ for _, sign in ipairs(signs) do
   vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
 end
 
-vim.g.mapleader = "\\"
-vim.g.maplocalleader = ","
+--------------------------------------------------------------------- telescope
 
-keymap("n", "<leader>1",       "<cmd>set number!<cr>", opts)
-keymap("n", "<leader>2",       "<cmd>set relativenumber!<cr>", opts)
-keymap("n", "<leader>3",       "<cmd>set hlsearch!<cr>", opts)
 keymap("n", "<leader>b",       "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
 keymap("n", "<leader>f",       "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
 keymap("n", "<leader>F",       "<cmd>lua require('telescope.builtin').find_files()<cr>", opts)
@@ -96,8 +66,6 @@ keymap("n", "<leader>r",       "<cmd>lua require('telescope.builtin').registers(
 keymap("n", "<leader>t",       "<cmd>lua require('telescope.builtin').tags()<cr>", opts)
 keymap("n", "<leader>\\",      "<cmd>lua require('telescope.builtin').file_browser()<cr>", opts)
 keymap("n", "<leader><space>", "<cmd>lua require('telescope.builtin').builtin()<cr>", opts)
-keymap("v", "*",               ":<C-u>call setreg('/', substitute(GetVisualSelection(), '\\_s\\+', '\\\\_s\\\\+', 'g'))<cr>n", opts)
-keymap("v", "#",               ":<C-u>call setreg('?', substitute(GetVisualSelection(), '\\_s\\+', '\\\\_s\\\\+', 'g'))<cr>n", opts)
 
 ----------------------------------------------------------------------- Comment
 
@@ -188,7 +156,7 @@ require('gitsigns').setup {
   end
 }
 
---------------------------------------------------------------------------- CMP
+--------------------------------------------------------------------------- cmp
 
 local cmp = require('cmp')
 
@@ -287,57 +255,3 @@ wk.register({
   ["<localleader>R"]  = { "<cmd>lua vim.lsp.buf.rename()<cr>",                                    "Rename" },
   ["<localleader>s"]  = { "<cmd>lua require('telescope.builtin').lsp_workspace_symbols()<cr>",    "List workspace symbols" }
 })
-
--------------------------------------------------------------------------------
-
-vim.cmd [[
-function! GetVisualSelection()
-  norm gv"sy
-  let l:ret = getreg('s')
-  exe "norm \<Esc>"
-  return l:ret
-endfunction
-
-" commands
-command! -count MakeTags !git tags
-command! -count Open     !open %
-
-" colors
-hi ColorColumn                      ctermbg=18
-hi DiffAdd                          ctermbg=2  ctermfg=0 cterm=BOLD
-hi DiffChange                       ctermbg=3  ctermfg=0 cterm=BOLD
-hi DiffDelete                       ctermbg=1  ctermfg=0 cterm=BOLD
-hi DiffText                         ctermbg=2  ctermfg=0 cterm=BOLD
-hi Directory                                   ctermfg=4
-hi GitSignsCurrentLineBlame                    ctermfg=8
-hi GitSignsAdd                      ctermbg=0  ctermfg=2
-hi GitSignsChange                   ctermbg=0  ctermfg=3
-hi GitSignsDelete                   ctermbg=0  ctermfg=1
-hi FoldColumn                       ctermbg=0  ctermfg=7
-hi Folded                           ctermbg=6  ctermfg=0
-hi LspDiagnosticsSignError          ctermbg=0  ctermfg=1
-hi LspDiagnosticsSignHint           ctermbg=0  ctermfg=7
-hi LspDiagnosticsSignInfo           ctermbg=0  ctermfg=4
-hi LspDiagnosticsSignWarning        ctermbg=0  ctermfg=16
-hi LspDiagnosticsVirtualTextError   ctermbg=0  ctermfg=1
-hi LspDiagnosticsVirtualTextHint    ctermbg=0  ctermfg=7
-hi LspDiagnosticsVirtualTextInfo    ctermbg=0  ctermfg=4
-hi LspDiagnosticsVirtualTextWarning ctermbg=0  ctermfg=16
-hi LspReferenceRead                 ctermbg=18 ctermfg=4
-hi LspReferenceWrite                ctermbg=18 ctermfg=11
-hi MatchParen                       ctermbg=18 ctermfg=2
-hi NormalFloat                      ctermbg=18 ctermfg=7
-hi Pmenu                            ctermbg=18 ctermfg=7
-hi PmenuSbar                        ctermbg=0  ctermfg=8
-hi Search                           ctermbg=2  ctermfg=0
-hi SignColumn                       ctermbg=0
-hi TabLine                          ctermbg=0  ctermfg=7 cterm=NONE
-hi TabLineFill                      ctermbg=0  ctermfg=0
-hi TabLineSel                       ctermbg=0  ctermfg=9 cterm=NONE
-hi VertSplit                        ctermbg=8  ctermfg=8
-hi Visual                           ctermbg=7  ctermfg=0
-
-" autocommands
-autocmd! BufWritePost * :silent! MakeTags
-autocmd! BufWritePre * :%s/\s\+$//e
-]]
--- share/common/.vimrc
+++ share/common/.vimrc
@@ -1,3 +1,140 @@
-source ~/.vim/init/general.vim
-source ~/.vim/init/coc.vim
-source ~/.vim/init/fzf.vim
+filetype plugin indent on
+syntax enable
+
+" functions
+function! GetSelectedText()
+  norm gv"sy
+  let l:ret = getreg('s')
+  exe "norm \<Esc>"
+  return l:ret
+endfunction
+
+function! MakeTags()
+  let tags_job = job_start("git ctags", #{ exit_cb: function('MakeTagsResult') })
+endfunction
+
+function! MakeTagsResult(job, status)
+  if a:status == 0
+    echom "MakeTags: done"
+  else
+    echom "MakeTags: tags generation failed"
+  endif
+endfunction
+
+" settings
+set backspace=indent,eol,start
+set clipboard=unnamedplus
+set cmdheight=1
+set colorcolumn=80,160
+set completeopt=menu,menuone,noselect
+set cursorline
+set diffopt+=algorithm:patience
+set diffopt+=indent-heuristic
+set diffopt-=internal
+set dir=/tmp
+set encoding=utf-8
+set encoding=utf-8
+set expandtab
+set exrc
+set foldcolumn=1
+set foldmethod=manual
+set grepformat=%f:%l:%c:%m
+set grepprg=rg\ --vimgrep\ $*
+set hidden
+set hlsearch
+set incsearch
+set laststatus=2
+set list
+set listchars=tab:»\ ,nbsp:␣,trail:·,extends:›,precedes:‹
+set mouse=a
+set nobackup
+set nocompatible
+set noshowmode
+set nospell
+set noswapfile
+set nowritebackup
+set number
+set path+=**
+set shiftwidth=2
+set shortmess+=c
+set showcmd
+set signcolumn=yes
+set softtabstop=2
+set splitright
+set tabstop=2
+set tags+=.git/tags;
+set termencoding=utf-8
+set timeoutlen=1000 ttimeoutlen=0
+set ttyfast
+set updatetime=300
+set wildignore=.*,.git/
+set wildmenu
+
+" options
+let g:mapleader = "\\"
+let g:maplocalleader = ","
+
+" mappings
+nnoremap <leader>1  :set number!<CR>
+nnoremap <leader>2  :set hlsearch!<CR>
+
+nmap <silent>       [a :previous<CR>
+nmap <silent>       ]a :next<CR>
+nmap <silent>       [b :bprevious<CR>
+nmap <silent>       ]b :bnext<CR>
+nmap <silent>       [w <C-w>W
+nmap <silent>       ]w <C-w>w
+nmap <silent>       [t :tabprevious<CR>
+nmap <silent>       ]t :tabnext<CR>
+nmap <silent>       [e :cprevious<CR>
+nmap <silent>       ]e :cnext<CR>
+nmap <silent>       [l :lprevious<CR>
+nmap <silent>       ]l :lnext<CR>
+
+vnoremap <silent>*  :<C-u>call setreg("/", substitute(GetSelectedText(), '\_s\+', '\\_s\\+', 'g'))<CR>n
+vnoremap <silent>#  :<C-u>call setreg("?", substitute(GetSelectedText(), '\_s\+', '\\_s\\+', 'g'))<CR>n
+vmap     v          <Plug>(expand_region_expand)
+vmap     <C-v>      <Plug>(expand_region_shrink)
+
+" commands
+command! -count MakeTags call MakeTags()
+command! -count Open     !open %
+
+" colors
+hi ColorColumn                      ctermbg=18
+hi DiffAdd                          ctermbg=2  ctermfg=0 cterm=BOLD
+hi DiffChange                       ctermbg=3  ctermfg=0 cterm=BOLD
+hi DiffDelete                       ctermbg=1  ctermfg=0 cterm=BOLD
+hi DiffText                         ctermbg=2  ctermfg=0 cterm=BOLD
+hi Directory                                   ctermfg=4
+hi GitSignsCurrentLineBlame                    ctermfg=8
+hi GitSignsAdd                      ctermbg=0  ctermfg=2
+hi GitSignsChange                   ctermbg=0  ctermfg=3
+hi GitSignsDelete                   ctermbg=0  ctermfg=1
+hi FoldColumn                       ctermbg=0  ctermfg=7
+hi Folded                           ctermbg=6  ctermfg=0
+hi LspDiagnosticsSignError          ctermbg=0  ctermfg=1
+hi LspDiagnosticsSignHint           ctermbg=0  ctermfg=7
+hi LspDiagnosticsSignInfo           ctermbg=0  ctermfg=4
+hi LspDiagnosticsSignWarning        ctermbg=0  ctermfg=16
+hi LspDiagnosticsVirtualTextError   ctermbg=0  ctermfg=1
+hi LspDiagnosticsVirtualTextHint    ctermbg=0  ctermfg=7
+hi LspDiagnosticsVirtualTextInfo    ctermbg=0  ctermfg=4
+hi LspDiagnosticsVirtualTextWarning ctermbg=0  ctermfg=16
+hi LspReferenceRead                 ctermbg=18 ctermfg=4
+hi LspReferenceWrite                ctermbg=18 ctermfg=11
+hi MatchParen                       ctermbg=18 ctermfg=2
+hi NormalFloat                      ctermbg=18 ctermfg=7
+hi Pmenu                            ctermbg=18 ctermfg=7
+hi PmenuSbar                        ctermbg=0  ctermfg=8
+hi Search                           ctermbg=2  ctermfg=0
+hi SignColumn                       ctermbg=0
+hi TabLine                          ctermbg=0  ctermfg=7 cterm=NONE
+hi TabLineFill                      ctermbg=0  ctermfg=0
+hi TabLineSel                       ctermbg=0  ctermfg=9 cterm=NONE
+hi VertSplit                        ctermbg=8  ctermfg=8
+hi Visual                           ctermbg=7  ctermfg=0
+
+" autocommands
+autocmd! BufWritePost * :silent! MakeTags
+autocmd! BufWritePre * :%s/\s\+$//e
