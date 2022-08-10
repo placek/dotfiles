10c58ae750f494feb3f4aa93672c153a76d1defa
Paweł Placzyński <placzynski.pawel@gmail.com>
2022-08-10 11:18:32 +0200

    [dotfiles] simpler way of managing dotfiles

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Overall configuration files of my own setup.";
 
   inputs = {
-    nixpkgs.url  = "github:NixOS/nixpkgs/9e86f5f7a19db6da2445f07bafa6694b556f9c6d";
+    nixpkgs.url     = "github:NixOS/nixpkgs/9e86f5f7a19db6da2445f07bafa6694b556f9c6d";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -13,27 +13,15 @@
         name         = "dotfiles";
         version      = "test";
         src          = ./.;
-        buildInputs  = [ pkgs.haskellPackages.mustache ];
-        buildPhase   = ''
-          export LANG=C.utf8
-          data_file="data.json"
-          [[ $(hostname) == "alpha" ]] && sed -i 's/"mobile":\s+true/"mobile": false'
-          for file in $(find . -name "*.mustache" -type f); do
-            target=$(echo $file | sed 's/\.mustache$//')
-            ${pkgs.haskellPackages.mustache}/bin/haskell-mustache $file $data_file > $target
-            rm -f $file
-          done
-        '';
         installPhase = ''
           mkdir -p $out
-          cp -r {bin,share,data.json} $out
+          cp -r {bin,share} $out
         '';
       };
     in rec {
-      # defaultApp     = flake-utils.lib.mkApp { drv = defaultPackage; };
       defaultPackage = dotfiles;
       devShell       = pkgs.mkShell {
-        buildInputs = [ dotfiles pkgs.nodePackages.bash-language-server pkgs.rnix-lsp pkgs.haskellPackages.mustache ];
+        buildInputs = [ dotfiles pkgs.nodePackages.bash-language-server pkgs.rnix-lsp ];
       };
     }
   );
